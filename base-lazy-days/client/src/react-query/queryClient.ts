3a016a8aab489e93d45a2f9a56e966c21eca168b
Potter, Jacob <Jacob.Potter@dynatrace.com>
2024-12-14 09:35:08 -0500

    init mutations for lazy days app

--- base-lazy-days/client/src/react-query/queryClient.ts
+++ base-lazy-days/client/src/react-query/queryClient.ts
@@ -1,7 +1,14 @@
-import { toast } from '@/components/app/toast'
-import { QueryCache, QueryClient } from '@tanstack/react-query'
+import { MutationCache, QueryCache, QueryClient } from '@tanstack/react-query'
 
-function errorHandler(errorMsg: string) {
+import { toast } from '@/components/app/toast'
+
+function createTitle(errorMsg: string, actionType: 'query' | 'mutation') {
+    return `could not ${actionType} data: ${
+        errorMsg ?? 'error connecting to server'
+    }`
+}
+
+function errorHandler(title: string) {
     // https://chakra-ui.com/docs/components/toast#preventing-duplicate-toast
     // one message per page load, not one message per query
     // the user doesn't care that there were three failed queries on the staff page
@@ -9,10 +16,6 @@ function errorHandler(errorMsg: string) {
     const id = 'react-query-toast'
 
     if (!toast.isActive(id)) {
-        const action = 'fetch'
-        const title = `could not ${action} data: ${
-            errorMsg ?? 'error connecting to server'
-        }`
         toast({
             id,
             title,
@@ -32,6 +35,11 @@ export const queryClient = new QueryClient({
         },
     },
     queryCache: new QueryCache({
+        onError: (error) => {
+            errorHandler(error.message)
+        },
+    }),
+    mutationCache: new MutationCache({
         onError: (error) => {
             errorHandler(error.message)
         },
